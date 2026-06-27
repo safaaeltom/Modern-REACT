8bd2ab4c1b7b8720c902ec62a532222dac71c284
Safaa <safaaeltom@gmail.com>
2026-06-27 14:20:13 +0300

    feat: implement click events in React components

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,11 +4,7 @@ const Navbar = () => {
           <h1>Modern React</h1>
           <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style={{
-                  color: "white",
-                  backgroundColor: '#f1356d',
-                  borderRadius: '8px'
-                }}>New blog</a>
+                <a href="/create">New blog</a>
           </div>
         </nav>
      );
