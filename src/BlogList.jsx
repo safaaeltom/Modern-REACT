e484fb2a0185e5b2881fa7720adb746e5394a489
Safaa <safaaeltom@gmail.com>
2026-06-28 16:09:28 +0300

    feat: learn to reuse components with filtered data using props

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -7,7 +7,6 @@ const BlogList = ({blogs, title}) => {
             <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author}</p>
-
             </div>
          ))}   
         </div>
