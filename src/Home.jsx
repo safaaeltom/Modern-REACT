0ee6054007da856aac62aa43b3825698f27ea692
Safaa <safaaeltom@gmail.com>
2026-07-02 12:26:58 +0300

    feat: set up JSON Server for a mock REST API

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,8 +8,6 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id:3 },
     ]);
 
-    const [name, setName] = useState('mario');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id!==id);
         setBlogs(newBlogs);
@@ -17,14 +15,11 @@ const Home = () => {
 
     useEffect(()=>{
         console.log('use effect ran')
-        console.log(name);
-    }, [name]);
+    }, []);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>Welcome, {name}!</p>
         </div>
      );
 }
