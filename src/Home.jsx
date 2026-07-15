20fba623e37fe9b8c40856e492eba83e29d1c995
Safaa <safaaeltom@gmail.com>
2026-07-15 13:44:54 +0300

    learn: experiment with delaying effects using setTimeout

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,27 +8,27 @@ const Home = () => {
     const[count, setCount] = useState(0);
 
     useEffect(()=>{
+        setTimeout (()=>{
         console.log('Effect ran');
         fetch('http://localhost:8000/blogs')
         .then(res => {
             return res.json();
         })
         .then(data => {
-            setTimeout (()=>{
             setBlogs(data);
             setIsLoading(false);
-            }, 3000);
         });
+        }, 3000);
     }, []);
 
     return ( 
         <div className="home">
+            {isLoading && <p>Loading ...</p>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
             <button onClick={()=>setName('Safaa')}>Change user</button>
             <p>{name}</p>
             <button onClick={()=>setCount(count + 1)}>Number of units</button>
             <p>{count}</p>
-            {isLoading && <p>Loading ...</p>}
         </div>
      );
 }
