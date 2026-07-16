9f9d02c4b1965a0b1fd5d89345d88f91687c2a65
Safaa <safaaeltom@gmail.com>
2026-07-17 00:02:21 +0300

    learn: extract fetch logic into a reusable custom hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,48 +1,15 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] =useState(null);
-    const[name, setName] = useState('Yousif');
-    const[count, setCount] = useState(0);
-
-    useEffect(()=>{
-      setTimeout (()=>{
-        fetch('http://localhost:8000/blogs')
-          .then(res => {
-            console.log(res);
-
-            if(!res.ok){
-              throw Error('could not fetch data')
-            }
-            return res.json();
-          })
-          .then(data => {
-            setBlogs(data);
-            setIsLoading(false);
-            setError(null);
-          })
-          .catch(err=>{
-            console.log(err);
-            console.log(err.message);
-
-            setError(err.message);
-            setIsLoading(false);
-          })
-      }, 3000);
-    }, []);
+    const { data:blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
             {isLoading && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
-            <button onClick={()=>setName('Safaa')}>Change user</button>
-            <p>{name}</p>
-            <button onClick={()=>setCount(count + 1)}>Number of units</button>
-            <p>{count}</p>
         </div>
      );
 }
---
+++ src/useFetch.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect} from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] =useState(null);
+
+    useEffect(()=>{
+      setTimeout (()=>{
+        fetch(url)
+          .then(res => {
+            if(!res.ok){
+              throw Error('could not fetch data')
+            }
+            return res.json();
+          })
+          .then(data => {
+            setData(data);
+            setIsLoading(false);
+            setError(null);
+          })
+          .catch(err=>{
+            setIsLoading(false);
+            setError(err.message);
+          })
+      }, 3000);
+    }, [url]);
+
+    return { data, isLoading, error }
+}
+
+ 
+export default useFetch;
