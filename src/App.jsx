b6d58b3010c7abe5ac4a4467e2e82767ddbb6445
Safaa <safaaeltom@gmail.com>
2026-07-22 13:10:04 +0300

    learn: configure BrowserRouter and Home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Navbar from './Navbar'
 import Home from './Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar />
+
       <div className="content">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
   );
 } 
 
