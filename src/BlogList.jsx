acefbd14bba1ad33551beab2d76871ffabf3a2e2
Safaa <safaaeltom@gmail.com>
2026-08-08 21:36:06 +0300

    style: enhance blog previews and refine blog display

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -8,8 +8,7 @@ const BlogList = ({blogs, title}) => {
             <div className="blog-preview" key={blog.id}>
                 <Link to={`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
-                    <p>{blog.body}</p>
-                    <p>Written by: {blog.author}</p>
+                    <p>Author: {blog.author}</p>
                 </Link>
             </div>
          ))}   
