0bb02306944c48c1c19eb1405cdfe392da725bdb
Safaa <safaaeltom@gmail.com>
2026-08-09 21:33:33 +0300

    feat: fetch individual blog by route id and display its details

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,13 +1,24 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch'
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const {data:blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id) 
 
     return ( 
-        <div className="blog-details">
-            <h2>Blog details - {id}</h2>
-        </div>
-     );
+      <div className="blog-details">
+        <h1>Blog Details</h1>
+        {error && <div>{error}</div>}
+        {isLoading && <div>Loading...</div>}
+        {blog && (
+            <article>
+               <h2>{blog.title}</h2>
+               <h3>Written by: {blog.author}</h3>
+               <p>{blog.body}</p>
+            </article>
+          )}
+      </div>
+    );
 }
  
 export default BlogDetails;
