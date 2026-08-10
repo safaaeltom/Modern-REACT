8ee3f597ee6c4b2efd6008931ef8537efa3dea86
Safaa <safaaeltom@gmail.com>
2026-08-10 14:37:50 +0300

    feat: manage blog title input with useState and onChange

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,12 +1,17 @@
+import {useState} from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
 
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
             <form>
                 <label>Blog title</label>
-                <input 
+                <input
+                value={title}
                 type="text" required
+                onChange={(e)=>setTitle(e.target.value)}
                 />
 
                 <label>Blog body</label>
@@ -19,6 +24,7 @@ const Create = () => {
                     <option value="yoshi">Yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title}</p>
             </form>
         </div>
      );
