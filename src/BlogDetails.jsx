c02656ae5e0d829ad4007ea804a08f67fddf1892
Safaa <safaaeltom@gmail.com>
2026-08-13 12:57:33 +0300

    feat: add DELETE request for blogs and return to home after deletion

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from './useFetch'
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const {data:blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id) 
+    const {data:blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id)
+    const navigate = useNavigate();
+    
+    const handleDelete=()=>{
+      fetch('http://localhost:8000/blogs/' + blog.id, {
+        method: 'DELETE'
+      }).then(()=>{
+        navigate('/');
+      })
+    }
 
     return ( 
       <div className="blog-details">
@@ -15,6 +24,7 @@ const BlogDetails = () => {
                <h2>{blog.title}</h2>
                <h3>Written by: {blog.author}</h3>
                <p>{blog.body}</p>
+               <button onClick={handleDelete}>Delete</button>
             </article>
           )}
       </div>
