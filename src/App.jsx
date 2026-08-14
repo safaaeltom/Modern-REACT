d4048ccac043c318535ad3e5045e6ae98e44dd7a
Safaa <safaaeltom@gmail.com>
2026-08-14 12:36:42 +0300

    Add not found page with homepage navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
           <Route path="/blogs/:id" element={<BlogDetails />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
+          <Route path="*" element={<NotFound />} />
+
+
         </Routes>
       </div>
     </div>
